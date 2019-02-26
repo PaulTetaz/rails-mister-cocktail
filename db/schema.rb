04ffee1b99627dcd04ac901b7d4d2a4cd6ac19f2
Paul Tetaz <paul.tetaz@yahoo.fr>
2019-02-26 18:30:20 +0000

    DONE

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2019_02_25_194255) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
- create_table "cocktails", force: :cascade do |t|
+  create_table "cocktails", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
